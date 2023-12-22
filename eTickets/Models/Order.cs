5aa22736b5e77ff8b4b6a695e9343aa2c30d54c0
Mehmet Can Girgin <119158579+Casm9@users.noreply.github.com>
2023-12-22 19:11:37 +0300

    updated database

--- eTickets/Models/Order.cs
+++ eTickets/Models/Order.cs
@@ -8,9 +8,6 @@ namespace eTickets.Models
 		[Key]
 		public int Id { get; set; }
 
-        public string Email { get; set; }
-		public string UserId { get; set; }
-
-        public List<OrderItem> OrderItems { get; set; }
-    }
+       		public List<OrderItem> OrderItems { get; set; }
+    	}
 }

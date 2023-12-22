572915abafee86908a87f30541730e2136fe88c7
Mehmet Can Girgin <119158579+Casm9@users.noreply.github.com>
2023-12-22 19:18:36 +0300

    undo a chance

--- eTickets/Models/Order.cs
+++ eTickets/Models/Order.cs
@@ -7,6 +7,9 @@ namespace eTickets.Models
 	{
 		[Key]
 		public int Id { get; set; }
+		
+		public string Email { get; set; }
+		public string UserId { get; set; }
 
        		public List<OrderItem> OrderItems { get; set; }
     	}
